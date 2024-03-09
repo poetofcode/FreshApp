e36e6798813fc37ea6ef95c861a6ad6f5e506513
poetofcode <cyber-post@yandex.ru>
2024-03-09 13:14:14 +0300

    Impl

--- composeApp/src/commonMain/kotlin/data/repository/FeedRepository.kt
+++ composeApp/src/commonMain/kotlin/data/repository/FeedRepository.kt
@@ -1,11 +1,8 @@
 package data.repository
 
-import data.entity.DataResponse
-import data.entity.ExceptionResponse
-import data.entity.FailureResponse
 import data.service.FreshApi
 import domain.model.PostModel
-import java.lang.Exception
+import repository.resultOrError
 
 interface FeedRepository {
 
@@ -13,33 +10,19 @@ interface FeedRepository {
 
 }
 
-class SomeNetworkException(val reason: String) : Exception(reason)
-
 class FeedRepositoryImpl(val api: FreshApi) : FeedRepository {
 
     override suspend fun fetchFeed(): List<PostModel> {
-        val feedResponse = api.fetchFeed()
-
-        return when (feedResponse) {
-            is DataResponse -> {
-                feedResponse.result.posts?.map { post ->
-                    PostModel(
-                        title = post.title.orEmpty(),
-                        image = post.image,
-                        link = post.link.orEmpty(),
-                        commentsCount = post.commentsCount ?: "0"
-                    )
-                } ?: emptyList()
-            }
-
-            is ExceptionResponse -> {
-                throw SomeNetworkException(feedResponse.error.message.orEmpty())
-            }
-
-            is FailureResponse -> {
-                throw SomeNetworkException(feedResponse.description)
-            }
-        }
+        val resultResponse = api.fetchFeed()
+        val parsed = resultResponse.resultOrError()
+        return parsed?.posts?.map { post ->
+                PostModel(
+                    title = post.title.orEmpty(),
+                    image = post.image,
+                    link = post.link.orEmpty(),
+                    commentsCount = post.commentsCount ?: "0"
+                )
+        } ?: emptyList()
     }
 
 }
