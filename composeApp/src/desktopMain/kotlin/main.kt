99d45dc15ff106fc46c90369249dfd92c8f86c4d
poetofcode <cyber-post@yandex.ru>
2024-03-16 22:54:35 +0300

    impl

--- composeApp/src/desktopMain/kotlin/main.kt
+++ composeApp/src/desktopMain/kotlin/main.kt
@@ -9,173 +9,21 @@ import androidx.compose.ui.window.rememberWindowState
 import data.repository.RepositoryFactoryImpl
 import data.service.NetworkingFactory
 import data.service.NetworkingFactoryImpl
+import data.utils.ContentBasedPersistentStorage
+import data.utils.FileContentProvider
+import data.utils.getValue
+import data.utils.setValue
 import dev.datlag.kcef.KCEF
 import kotlinx.coroutines.Dispatchers
 import kotlinx.coroutines.flow.launchIn
 import kotlinx.coroutines.flow.onEach
 import kotlinx.coroutines.withContext
-import kotlinx.serialization.KSerializer
-import kotlinx.serialization.Serializable
-import kotlinx.serialization.descriptors.SerialDescriptor
-import kotlinx.serialization.descriptors.buildClassSerialDescriptor
-import kotlinx.serialization.encodeToString
-import kotlinx.serialization.encoding.Decoder
-import kotlinx.serialization.encoding.Encoder
-import kotlinx.serialization.json.*
-import kotlinx.serialization.json.Json.Default.encodeToString
 import presentation.App
 import presentation.base.Config
 import presentation.base.ViewModelStore
 import presentation.factories.viewModelFactories
 import java.io.File
 import kotlin.math.max
-import kotlin.reflect.KProperty
-
-
-@Serializable
-data class PreferencesInfo(
-    val preferences: Map<String, @kotlinx.serialization.Serializable(with = AnySerializer::class) Any>? = null
-)
-
-object AnySerializer : KSerializer<Any> {
-    override val descriptor: SerialDescriptor = buildClassSerialDescriptor("Any")
-
-    override fun serialize(encoder: Encoder, value: Any) {
-        val jsonEncoder = encoder as JsonEncoder
-        val jsonElement = serializeAny(value)
-        jsonEncoder.encodeJsonElement(jsonElement)
-    }
-
-    private fun serializeAny(value: Any?): JsonElement = when (value) {
-        is Map<*, *> -> {
-            val mapContents = value.entries.associate { mapEntry ->
-                mapEntry.key.toString() to serializeAny(mapEntry.value)
-            }
-            JsonObject(mapContents)
-        }
-
-        is List<*> -> {
-            val arrayContents = value.map { listEntry -> serializeAny(listEntry) }
-            JsonArray(arrayContents)
-        }
-
-        is Number -> JsonPrimitive(value)
-        is Boolean -> JsonPrimitive(value)
-        else -> JsonPrimitive(value.toString())
-    }
-
-    override fun deserialize(decoder: Decoder): Any {
-        val jsonDecoder = decoder as JsonDecoder
-        val element = jsonDecoder.decodeJsonElement()
-
-        return deserializeJsonElement(element)
-    }
-
-    private fun deserializeJsonElement(element: JsonElement): Any = when (element) {
-        is JsonObject -> {
-            element.mapValues { deserializeJsonElement(it.value) }
-        }
-
-        is JsonArray -> {
-            element.map { deserializeJsonElement(it) }
-        }
-
-        is JsonPrimitive -> element.toString()
-    }
-}
-
-
-interface ContentProvider {
-
-    fun provideContent(): String
-
-    fun saveContent(content: String)
-
-}
-
-class FileContentProvider(
-    val fileName: String,
-    val relativePath: String,
-) : ContentProvider {
-    override fun provideContent(): String {
-        val cachePath = File("./", relativePath)
-        cachePath.mkdirs()
-        val stream = File("$cachePath/$fileName").bufferedReader()
-        return stream.use { it.readText() }
-    }
-
-    override fun saveContent(content: String) {
-        val cachePath = File("./", relativePath)
-        cachePath.mkdirs()
-        val stream = File("$cachePath/$fileName")
-        stream.printWriter().use {
-            it.write(content)
-        }
-    }
-
-}
-
-
-interface PersistentStorage {
-
-    fun save(key: String, param: Any)
-
-    fun fetch(key: String): Any?
-
-}
-
-class DesktopPersistentStorage(
-    private val contentPorvider: ContentProvider
-) : PersistentStorage {
-
-    private val json by lazy {
-        Json {
-            ignoreUnknownKeys = true
-        }
-    }
-
-    private val map: MutableMap<String, Any> by lazy {
-        val content = try {
-            contentPorvider.provideContent()
-        } catch (e: Throwable) {
-            "{}"
-        }
-        val map = json.decodeFromString<PreferencesInfo>(content).preferences?.toMutableMap() ?: mutableMapOf()
-        map.toMap().toMutableMap()
-    }
-
-
-    override fun save(key: String, param: Any) {
-        map[key] = param
-
-        val str = json.encodeToString(PreferencesInfo(map))
-        contentPorvider.saveContent(str)
-    }
-
-    override fun fetch(key: String): Any? {
-        return map[key]
-    }
-}
-
-
-inline operator fun <reified T : Any> PersistentStorage.getValue(nothing: Nothing?, property: KProperty<*>): T? {
-    val properyName = property.name
-    val res = fetch(properyName)
-    return when (T::class) {
-        String::class -> res?.toString() as? T
-        Int::class -> res?.toString()?.toIntOrNull() as? T
-
-        else -> null
-    }
-}
-
-inline operator fun <reified T : Any> PersistentStorage.setValue(nothing: Nothing?, property: KProperty<*>, value: T?) {
-    val propertyName = property.name
-    value?.let {
-        this.save(propertyName, value)
-    }
-}
-
 
 
 fun main() = application {
@@ -196,7 +44,7 @@ fun main() = application {
         relativePath = "appcache",
     )
 
-    val storage = DesktopPersistentStorage(fileProvider)
+    val storage = ContentBasedPersistentStorage(fileProvider)
 
     var windowWidth: Int? by storage
     var windowHeight: Int? by storage
