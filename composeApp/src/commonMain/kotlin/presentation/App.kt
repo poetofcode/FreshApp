f619c1d6af9c1bdda1d1cad8fa4ab09b156ccfa4
poetofcode <cyber-post@yandex.ru>
2024-03-17 12:58:01 +0300

    impl

--- composeApp/src/commonMain/kotlin/presentation/App.kt
+++ composeApp/src/commonMain/kotlin/presentation/App.kt
@@ -1,5 +1,6 @@
 package presentation
 
+import androidx.compose.foundation.Image
 import androidx.compose.foundation.background
 import androidx.compose.foundation.clickable
 import androidx.compose.foundation.layout.*
@@ -11,9 +12,15 @@ import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.unit.dp
+import freshapp.composeapp.generated.resources.Res
+import freshapp.composeapp.generated.resources.compose_multiplatform
+import freshapp.composeapp.generated.resources.ic_home_tab
+import freshapp.composeapp.generated.resources.ic_profile_tab
 import presentation.base.Config
 import org.jetbrains.compose.resources.ExperimentalResourceApi
+import org.jetbrains.compose.resources.painterResource
 import org.jetbrains.compose.ui.tooling.preview.Preview
+import presentation.Tabs.*
 import presentation.navigation.NavState
 
 import presentation.navigation.NavStateImpl
@@ -36,6 +43,18 @@ fun App(config: Config) {
                     navState.moveToFront(tab.key)
                 },
                 itemContent = { tab, isSelected ->
+                    Box(Modifier.size(30.dp, 30.dp), contentAlignment = Alignment.Center) {
+                        val icon = when (tab) {
+                            HOME -> Res.drawable.ic_home_tab
+                            PROFILE -> Res.drawable.ic_profile_tab
+                        }
+                        Image(
+                            painter = painterResource(icon),
+                            contentDescription = null
+                        )
+                    }
+
+                    /*
                     Button(
                         modifier = Modifier.padding(20.dp),
                         onClick = {
@@ -48,6 +67,7 @@ fun App(config: Config) {
                         }
                         Text(text = text)
                     }
+                    */
                 }
             ),
         ) {
@@ -60,7 +80,7 @@ fun App(config: Config) {
         LaunchedEffect(Unit) {
             navState.push(HomeTabScreen())
             // navState.push(ProfileTabScreen())
-            navState.moveToFront(Tabs.HOME.key)
+            navState.moveToFront(HOME.key)
         }
     }
 }
@@ -100,7 +120,10 @@ fun AppLayout(
                 modifier = Modifier.fillMaxHeight().background(Color.LightGray),
             ) {
                 menu.tabs.forEach { tab ->
-                    Box(Modifier.clickable { menu.onTabClick(tab) }) {
+                    Box(
+                        modifier = Modifier.clickable { menu.onTabClick(tab) }.size(60.dp),
+                        contentAlignment = Alignment.Center,
+                    ) {
                         menu.itemContent(tab, false)
                     }
                 }
