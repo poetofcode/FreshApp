bafdb0d3be616ff29f47ae325641c0162d99e23a
uncorner <denis.uncorner@gmail.com>
2024-03-25 20:43:52 +0300

    pass Modifier for AndroidImageUtil

--- composeApp/src/androidMain/kotlin/com/poetofcode/freshapp/utils/AndroidImageUtil.kt
+++ composeApp/src/androidMain/kotlin/com/poetofcode/freshapp/utils/AndroidImageUtil.kt
@@ -1,12 +1,13 @@
 package com.poetofcode.freshapp
 
 import androidx.compose.runtime.Composable
+import androidx.compose.ui.Modifier
 import coil.compose.AsyncImage
 
 internal class AndroidImageUtil {
 
     @Composable
-    fun AsyncImage(url: String) {
-        AsyncImage(model = url, contentDescription = null)
+    fun AsyncImage(url: String, modifier: Modifier) {
+        AsyncImage(model = url, contentDescription = null, modifier = modifier)
     }
 }
