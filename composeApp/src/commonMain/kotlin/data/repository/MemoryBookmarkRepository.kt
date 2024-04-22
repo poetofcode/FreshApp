5efd243649b13384b4378aa2e114381f623b2944
uncorner <denis.uncorner@gmail.com>
2024-04-22 20:31:52 +0300

    temp fix removing in repositore

--- composeApp/src/commonMain/kotlin/data/repository/MemoryBookmarkRepository.kt
+++ composeApp/src/commonMain/kotlin/data/repository/MemoryBookmarkRepository.kt
@@ -5,16 +5,23 @@ import domain.model.PostModel
 class MemoryBookmarkRepository : BookmarkRepository {
 
     //private val posts: ArrayList<PostModel> = arrayListOf()
-    private val posts: MutableList<PostModel> = mutableListOf()
+    //private val posts: MutableList<PostModel> = mutableListOf()
+    private var posts: MutableList<PostModel> = mutableListOf()
 
     override fun add(post: PostModel) {
         posts.add(post)
     }
 
     override fun remove(id: String): List<PostModel> {
-        posts.removeIf{
-            it.id == id
+//        posts.removeIf{
+//            it.id == id
+//        }
+//        return posts
+
+        val updPosts = posts.filter {
+            it.id != id
         }
+        posts = updPosts.toMutableList()
         return posts
     }
 
