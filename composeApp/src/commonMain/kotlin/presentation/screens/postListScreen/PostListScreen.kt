9173cc26123fe8c306658582fcf5b14f4a194f26
poetofcode <cyber-post@yandex.ru>
2024-08-19 00:27:32 +0300

    Adaptive grid

--- composeApp/src/commonMain/kotlin/presentation/screens/postListScreen/PostListScreen.kt
+++ composeApp/src/commonMain/kotlin/presentation/screens/postListScreen/PostListScreen.kt
@@ -1,12 +1,33 @@
 package presentation.screens.postListScreen
 
-import androidx.compose.foundation.*
-import androidx.compose.foundation.layout.*
-import androidx.compose.foundation.lazy.LazyColumn
+import androidx.compose.foundation.ExperimentalFoundationApi
+import androidx.compose.foundation.background
+import androidx.compose.foundation.clickable
+import androidx.compose.foundation.layout.Arrangement
+import androidx.compose.foundation.layout.Box
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.PaddingValues
+import androidx.compose.foundation.layout.Row
+import androidx.compose.foundation.layout.Spacer
+import androidx.compose.foundation.layout.fillMaxHeight
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.layout.height
+import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.layout.size
+import androidx.compose.foundation.layout.width
 import androidx.compose.foundation.lazy.LazyListState
-import androidx.compose.foundation.lazy.items
+import androidx.compose.foundation.lazy.grid.GridCells
+import androidx.compose.foundation.lazy.grid.LazyVerticalGrid
+import androidx.compose.foundation.lazy.grid.items
+import androidx.compose.foundation.lazy.grid.rememberLazyGridState
 import androidx.compose.foundation.shape.RoundedCornerShape
-import androidx.compose.material.*
+import androidx.compose.material.CircularProgressIndicator
+import androidx.compose.material.Icon
+import androidx.compose.material.IconButton
+import androidx.compose.material.MaterialTheme
+import androidx.compose.material.Text
+import androidx.compose.material.TopAppBar
 import androidx.compose.material.icons.Icons
 import androidx.compose.material.icons.filled.Refresh
 import androidx.compose.runtime.Composable
@@ -36,7 +57,6 @@ class PostListScreen : BaseScreen<PostListViewModel>() {
     override val isMenuVisible: Boolean = true
 
     private val listState = LazyListState()
-
 
     @Composable
     override fun Content() = with(viewModel.state.value) {
@@ -90,14 +110,16 @@ class PostListScreen : BaseScreen<PostListViewModel>() {
     @OptIn(ExperimentalFoundationApi::class)
     @Composable
     private fun Posts(posts: List<PostModel>) {
+        val gridState = rememberLazyGridState()
         Row(
             modifier = Modifier.fillMaxSize(),
             verticalAlignment = Alignment.CenterVertically,
             horizontalArrangement = Arrangement.Center,
         ) {
-            LazyColumn(
-                state = listState,
+            LazyVerticalGrid(
                 modifier = Modifier.fillMaxHeight().padding().weight(1f),
+                state = gridState,
+                columns = GridCells.Adaptive(minSize = 300.dp),
                 contentPadding = PaddingValues(horizontal = 16.dp)
             ) {
                 items(posts) { post ->
