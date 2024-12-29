da757c4fd3efb032109304991a1c78aa6304c18b
poetofcode <cyber-post@yandex.ru>
2024-12-29 20:42:02 +0300

    Local fav: implementing

--- client/composeApp/src/commonMain/kotlin/data/repository/FavoriteRepository.kt
+++ client/composeApp/src/commonMain/kotlin/data/repository/FavoriteRepository.kt
@@ -2,10 +2,22 @@ package data.repository
 
 import domain.model.PostModel
 
-class FavoriteRepository {
+interface FavoriteRepository {
 
-    suspend fun add(post: PostModel) {
+    suspend fun add(post: PostModel)
 
+    suspend fun remove(id: String)
+
+}
+
+class FavoriteRepositoryImpl : FavoriteRepository {
+
+    override suspend fun add(post: PostModel) {
+        TODO("Not yet implemented")
+    }
+
+    override suspend fun remove(id: String) {
+        TODO("Not yet implemented")
     }
 
 }
--- client/composeApp/src/commonMain/kotlin/domain/model/PostModel.kt
+++ client/composeApp/src/commonMain/kotlin/domain/model/PostModel.kt
@@ -1,9 +1,13 @@
 package domain.model
 
+import data.utils.toSha1
+
 data class PostModel(
     val title: String,
     val image: String?,
     val link: String,
     val commentsCount: String,
     val isFavorite: Boolean,
-)
+) {
+    val id : String get() = link.toSha1()
+}
