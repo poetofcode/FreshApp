f2df2df8ac54a01e975246fbc89d6b2279877236
poetofcode <cyber-post@yandex.ru>
2025-01-01 16:11:39 +0300

    Local fav: implementing

--- client/composeApp/src/commonMain/kotlin/data/utils/AppDataStorage.kt
+++ client/composeApp/src/commonMain/kotlin/data/utils/AppDataStorage.kt
@@ -1,6 +1,8 @@
 package data.utils
 
 import domain.model.FavoritePost
+import domain.model.LocalAppData
+import kotlinx.serialization.encodeToString
 
 interface AppDataStorage {
 
@@ -14,12 +16,53 @@ class AppDataStorageImpl(
     private val contentProvider: ContentProvider
 ) : AppDataStorage {
 
-    override suspend fun saveFavorites(favorites: List<FavoritePost>) {
+    private val json by lazy { JsonProvider.json }
 
+    private var cache: LocalAppData? = null
+
+    override suspend fun saveFavorites(favorites: List<FavoritePost>) {
+        if (cache == null) {
+            saveAppData(LocalAppData(
+                favoritePosts = favorites
+            ))
+        } else {
+            cache?.let {
+                saveAppData(it.copy(
+                    favoritePosts = favorites
+                ))
+            }
+        }
     }
 
     override suspend fun fetchFavorites(): List<FavoritePost> {
-        return emptyList()
+        return if (cache == null) {
+            fetchAppData()?.favoritePosts.orEmpty()
+        } else {
+            cache?.favoritePosts.orEmpty()
+        }
+    }
+
+    private suspend fun fetchAppData() : LocalAppData? {
+        return try {
+            val fileContent = contentProvider.provideContent()
+            val data = json.decodeFromString<LocalAppData>(fileContent)
+            data.also {
+                cache = data
+            }
+        } catch (e: Throwable) {
+            // e.printStackTrace()
+            null
+        }
+    }
+
+    private suspend fun saveAppData(data: LocalAppData) {
+        try {
+            val fileContent = json.encodeToString<LocalAppData>(data)
+            contentProvider.saveContent(fileContent)
+            cache = data
+        } catch (e: Throwable) {
+            e.printStackTrace()
+        }
     }
 
 }
