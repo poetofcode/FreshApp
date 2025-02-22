8851cc612d026d58129273069648c6e862f9a4a5
poetofcode <cyber-post@yandex.ru>
2025-02-22 17:34:31 +0300

    Dashboard: implementing

--- client/composeApp/src/commonMain/kotlin/presentation/screens/postListScreen/PostListScreen.kt
+++ client/composeApp/src/commonMain/kotlin/presentation/screens/postListScreen/PostListScreen.kt
@@ -1,9 +1,15 @@
+@file:OptIn(ExperimentalLayoutApi::class)
+
 package presentation.screens.postListScreen
 
+import androidx.compose.foundation.background
+import androidx.compose.foundation.border
 import androidx.compose.foundation.clickable
 import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.ExperimentalLayoutApi
+import androidx.compose.foundation.layout.FlowRow
 import androidx.compose.foundation.layout.Row
 import androidx.compose.foundation.layout.Spacer
 import androidx.compose.foundation.layout.fillMaxSize
@@ -11,6 +17,7 @@ import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.layout.size
 import androidx.compose.foundation.lazy.grid.LazyGridState
+import androidx.compose.foundation.shape.RoundedCornerShape
 import androidx.compose.material.icons.Icons
 import androidx.compose.material.icons.automirrored.filled.ArrowBack
 import androidx.compose.material.icons.automirrored.filled.List
@@ -19,13 +26,16 @@ import androidx.compose.material3.CircularProgressIndicator
 import androidx.compose.material3.ExperimentalMaterial3Api
 import androidx.compose.material3.Icon
 import androidx.compose.material3.IconButton
+import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.Surface
 import androidx.compose.material3.Text
 import androidx.compose.material3.TopAppBar
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.draw.clip
 import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.graphics.Shape
 import androidx.compose.ui.unit.dp
 import domain.model.CategoryModel
 import freshapp.composeapp.generated.resources.Res
@@ -181,24 +191,90 @@ class PostListScreen : BaseScreen<PostListViewModel>() {
 
     @Composable
     private fun DialogTitle(text: String) {
-
+        Text(text = text)
     }
 
     @Composable
     private fun SourceList(sources: List<String>) {
-        for (source in sources) {
-            SourceItem(source)
-        }
+//        FlowRow {
+//            for (source in sources) {
+//                SourceItem(source)
+//            }
+//        }
     }
 
 
     @Composable
     private fun SourceItem(source: String) {
-        //
+
     }
 
     @Composable
     private fun CategoryItem(category: CategoryModel) {
-        // TODO("Not yet implemented")
+        // Text(text = category.title)
+        RoundedButton(title = category.title) { }
     }
+
+    @Composable
+    private fun RoundedButton(
+        modifier: Modifier = Modifier,
+        title: String = "Button",
+        textColor: Color? = null,
+        solidColor: Color? = null,
+        borderColor: Color? = null,
+        shape: Shape? = null,
+        onClick: () -> Unit,
+    ) {
+        Box(modifier = RoundedButtonDefaults.defaultRoundedModifier(
+            solidColor = solidColor,
+            borderColor = borderColor,
+            shape = shape,
+        ).then(modifier).clickable {
+            onClick()
+        }) {
+            Text(
+                text = title,
+                color = textColor ?: RoundedButtonDefaults.defaultTextColor()
+            )
+        }
+    }
+
+
+}
+
+
+object RoundedButtonDefaults {
+
+    @Composable
+    fun defaultRoundedModifier(
+        solidColor: Color? = null,
+        borderColor: Color? = null,
+        shape: Shape? = null,
+    ) = Modifier
+        .border(
+            width = 1.dp,
+            color = borderColor ?: defaultBorderColor(),
+            shape = shape ?: defaultShape()
+        )
+        .background(
+            color = solidColor ?: defaultSolidColor(),
+            shape = shape ?: defaultShape()
+        ).padding(
+            horizontal = 8.dp,
+            vertical = 4.dp
+        )
+
+    @Composable
+    fun defaultBorderColor() = MaterialTheme.colorScheme.onSurface.copy(
+        alpha = 0.5f
+    )
+
+    @Composable
+    fun defaultSolidColor() = MaterialTheme.colorScheme.surfaceBright
+
+    @Composable
+    fun defaultTextColor() = MaterialTheme.colorScheme.onSurface
+
+    @Composable
+    fun defaultShape() = RoundedCornerShape(size = 8.dp)
 }
